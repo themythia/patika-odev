bcaf9811bce9b719063916026600a55d6c46f568
Emir Al <mythia@windowslive.com>
2022-04-19 15:17:19 +0300

    add: logic for app

--- asiankitchen/app.js
+++ asiankitchen/app.js
@@ -1,83 +1,148 @@
 const menu = [
   {
     id: 1,
-    title: "Tteokbokki",
-    category: "Korea",
+    title: 'Tteokbokki',
+    category: 'Korea',
     price: 10.99,
-    img:
-      "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+    img: 'https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg',
     desc: `Spicy rice cakes, serving with fish cake.`,
   },
   {
     id: 2,
-    title: "Chicken Ramen",
-    category: "Japan",
+    title: 'Chicken Ramen',
+    category: 'Japan',
     price: 7.99,
-    img:
-      "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+    img: 'https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg',
     desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
   },
   {
     id: 3,
-    title: "Bibimbap",
-    category: "Korea",
+    title: 'Bibimbap',
+    category: 'Korea',
     price: 8.99,
-    img:
-      "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+    img: 'https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg',
     desc: `Boiling vegetables, serving with special hot sauce`,
   },
   {
     id: 4,
-    title: "Dan Dan Mian",
-    category: "China",
+    title: 'Dan Dan Mian',
+    category: 'China',
     price: 5.99,
-    img:
-      "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+    img: 'https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg',
     desc: `Dan dan noodle, serving with green onion `,
   },
   {
     id: 5,
-    title: "Yangzhou Fried Rice",
-    category: "China",
+    title: 'Yangzhou Fried Rice',
+    category: 'China',
     price: 12.99,
-    img:
-      "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+    img: 'https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg',
     desc: `Yangzhou style fried rice, serving with bean and pickles `,
   },
   {
     id: 6,
-    title: "Onigiri",
-    category: "Japan",
+    title: 'Onigiri',
+    category: 'Japan',
     price: 9.99,
-    img:
-      "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+    img: 'https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg',
     desc: `Rice Sandwich, serving with soy sauce`,
   },
   {
     id: 7,
-    title: "Jajangmyeon",
-    category: "Korea",
+    title: 'Jajangmyeon',
+    category: 'Korea',
     price: 15.99,
-    img:
-      "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+    img: 'https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg',
     desc: `Black bean sauce noodle, serving with green onion `,
   },
   {
     id: 8,
-    title: "Ma Yi Shang Shu",
-    category: "China",
+    title: 'Ma Yi Shang Shu',
+    category: 'China',
     price: 12.99,
-    img:
-      "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+    img: 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg',
     desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
   },
   {
     id: 9,
-    title: "Doroyaki",
-    category: "Japan",
+    title: 'Doroyaki',
+    category: 'Japan',
     price: 3.99,
-    img:
-      "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+    img: 'https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg',
     desc: `Red bean paste dessert, serving with honey.`,
   },
 ];
+
+const btnContainer = document.getElementsByClassName('btn-container')[0];
+const section = document.getElementsByClassName('section-center')[0];
+
+const categories = menu.reduce(
+  (categories, next) => {
+    if (!categories.includes(next.category)) categories.push(next.category);
+    return categories;
+  },
+  ['All']
+);
+
+let mealsArray = menu;
+section.innerHTML = mealsArray
+  .map(
+    (meal) => `<div class="menu-items col-lg-6 col-sm-12">
+                <img
+                  src=${meal.img}
+                  alt=${meal.title}
+                  class="photo"
+                />
+                <div class="menu-info">
+                  <div class="menu-title">
+                    <h4>${meal.title}</h4>
+                    <h4 class="price">${meal.price}</h4>
+                  </div>
+                  <div class="menu-text">
+                    ${meal.desc}
+                  </div>
+                </div>
+              </div>
+        `
+  )
+  .join('');
+
+let buttonState = 'All';
+
+categories.forEach((category) => {
+  let button = document.createElement('button');
+  button.innerHTML = category;
+  button.id = `btn-${category}`;
+  button.classList.add('btn', 'btn-outline-dark', 'btn-item');
+
+  btnContainer.appendChild(button);
+  button.addEventListener('click', () => {
+    buttonState = category;
+    mealsArray =
+      category === 'All'
+        ? menu
+        : menu.filter((meal) => meal.category === category);
+    let mealsToDisplay = mealsArray
+      .map(
+        (meal) => `<div class="menu-items col-lg-6 col-sm-12">
+                <img
+                  src=${meal.img}
+                  alt=${meal.title}
+                  class="photo"
+                />
+                <div class="menu-info">
+                  <div class="menu-title">
+                    <h4>${meal.title}</h4>
+                    <h4 class="price">${meal.price}</h4>
+                  </div>
+                  <div class="menu-text">
+                    ${meal.desc}
+                  </div>
+                </div>
+              </div>
+        `
+      )
+      .join('');
+    section.innerHTML = mealsToDisplay;
+  });
+});
